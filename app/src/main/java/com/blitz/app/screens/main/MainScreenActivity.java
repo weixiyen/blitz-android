b836d212a7d5ae07ca622eba4a6c9058dddebb15
mrkcsc <miguel@miguelgaeta.com>
2014-06-23 23:08:52 -0700

    Re-styled main screen to match Blitz screen.

--- app/src/main/java/com/blitz/app/screens/main/MainScreenActivity.java
+++ app/src/main/java/com/blitz/app/screens/main/MainScreenActivity.java
@@ -2,11 +2,8 @@ package com.blitz.app.screens.main;
 
 import android.content.Intent;
 
-import com.blitz.app.R;
 import com.blitz.app.base.activity.BaseActivity;
 import com.blitz.app.screens.post.PostScreenActivity;
-
-import butterknife.OnClick;
 
 public class MainScreenActivity extends BaseActivity {
 
@@ -23,18 +20,5 @@ public class MainScreenActivity extends BaseActivity {
         Intent intent = new Intent(this, PostScreenActivity.class);
 
         startActivity(intent);
-    }
-
-    //==============================================================================================
-    // Click Methods
-    //==============================================================================================
-
-    /**
-     * Register user and take them to main screen.
-     */
-    @OnClick(R.id.main_screen_post) @SuppressWarnings("unused")
-    public void postFavor() {
-
-        transitionNextScreen();
     }
 }
