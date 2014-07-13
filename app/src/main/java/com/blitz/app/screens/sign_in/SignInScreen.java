960261651c67b16416eee896758db31a71f3e925
mrkcsc <miguel@miguelgaeta.com>
2014-07-12 19:08:12 -0700

    Added id and hookup for sign in edit text fields.

--- app/src/main/java/com/blitz/app/screens/sign_in/SignInScreen.java
+++ app/src/main/java/com/blitz/app/screens/sign_in/SignInScreen.java
@@ -1,17 +1,27 @@
 package com.blitz.app.screens.sign_in;
 
 import android.content.Intent;
+import android.widget.EditText;
 
 import com.blitz.app.R;
 import com.blitz.app.screens.main.MainScreen;
 import com.blitz.app.utilities.android.BaseActivity;
 
+import butterknife.InjectView;
 import butterknife.OnClick;
 
 /**
  * Created by Miguel Gaeta on 6/1/14.
  */
 public class SignInScreen extends BaseActivity {
+
+    //==============================================================================================
+    // Member Variables
+    //==============================================================================================
+
+    // Mapped views.
+    @InjectView(R.id.sign_in_screen_username_or_email) EditText mEmailOrUsername;
+    @InjectView(R.id.sign_in_screen_password)          EditText mPassword;
 
     //==============================================================================================
     // Click Methods
