a0556c7bd3a8f7884fab06085dd92825ce97c237
mrkcsc <miguel@miguelgaeta.com>
2014-07-14 23:54:19 -0700

    Added button for toggling sound in the UI of main settings page.

--- app/src/main/java/com/blitz/app/screens/main/MainScreenFragmentSettings.java
+++ app/src/main/java/com/blitz/app/screens/main/MainScreenFragmentSettings.java
@@ -1,9 +1,21 @@
 package com.blitz.app.screens.main;
 
+import com.blitz.app.R;
 import com.blitz.app.utilities.android.BaseFragment;
+
+import butterknife.OnClick;
 
 /**
  * Created by mrkcsc on 7/14/14.
  */
 public class MainScreenFragmentSettings extends BaseFragment {
+
+    //==============================================================================================
+    // Click Methods
+    //==============================================================================================
+
+    @OnClick(R.id.main_screen_fragment_settings_toggle_music) @SuppressWarnings("unused")
+    public void toggleMusic() {
+
+    }
 }
