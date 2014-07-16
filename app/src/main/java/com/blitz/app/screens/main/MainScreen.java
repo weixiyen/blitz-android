4cd06e0010ee67ed45c94392ac268554dffdf43d
mrkcsc <miguel@miguelgaeta.com>
2014-07-16 00:19:36 -0700

    Switched from action bar to a custom widget for navigation.

--- app/src/main/java/com/blitz/app/screens/main/MainScreen.java
+++ app/src/main/java/com/blitz/app/screens/main/MainScreen.java
@@ -1,32 +1,33 @@
 package com.blitz.app.screens.main;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
 
 import com.blitz.app.R;
 import com.blitz.app.utilities.android.BaseActivity;
 import com.blitz.app.utilities.viewpager.ViewPagerDepthTransformer;
 
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by mrkcsc on 7/14/14.
  */
-public class MainScreen extends BaseActivity implements ActionBar.TabListener {
+public class MainScreen extends BaseActivity {
 
     //==============================================================================================
     // Member Variables
     //==============================================================================================
 
+    // Fragment view pager.
     @InjectView(R.id.main_screen_pager) ViewPager mPager;
 
-    // Adapter for view pager.
-    private MainScreenPagerAdapter mAdapter;
-
-    // Action bar.
-    private ActionBar mActionBar;
+    // Navigation bar tabs.
+    @InjectView(R.id.main_screen_nav_featured_active) View mNavFeaturedActive;
+    @InjectView(R.id.main_screen_nav_recent_active)   View mNavRecentActive;
+    @InjectView(R.id.main_screen_nav_settings_active) View mNavSettingsActive;
 
     //==============================================================================================
     // Overwritten Methods
@@ -43,9 +44,6 @@ public class MainScreen extends BaseActivity implements ActionBar.TabListener {
 
         // Setup view pager.
         setupViewPager();
-
-        // Setup view pager tabs.
-        setupViewPagerTabs();
     }
 
     /**
@@ -63,27 +61,6 @@ public class MainScreen extends BaseActivity implements ActionBar.TabListener {
         }
     }
 
-    /**
-     * When a tab is selected update
-     *
-     * the view pager.
-     *
-     * @param tab Selected tab.
-     * @param ft Fragment transaction.
-     */
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-        // Sync with view pager when selected.
-        mPager.setCurrentItem((Integer) tab.getTag());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) { }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) { }
-
     //==============================================================================================
     // Private Methods
     //==============================================================================================
@@ -95,10 +72,11 @@ public class MainScreen extends BaseActivity implements ActionBar.TabListener {
     private void setupViewPager() {
 
         // Create adapter.
-        mAdapter = new MainScreenPagerAdapter(getSupportFragmentManager());
+        MainScreenPagerAdapter adapter = new
+                MainScreenPagerAdapter(getSupportFragmentManager());
 
         // Create adapter for the view pager.
-        mPager.setAdapter(mAdapter);
+        mPager.setAdapter(adapter);
 
         // Add a custom page transition effect.
         mPager.setPageTransformer(true, new ViewPagerDepthTransformer());
@@ -113,38 +91,69 @@ public class MainScreen extends BaseActivity implements ActionBar.TabListener {
             @Override
             public void onPageSelected(int i) {
 
-                // Set action bar item.
-                mActionBar.setSelectedNavigationItem(i);
+                deselectNav();
+
+                switch (i) {
+                    case 0:
+                        mNavFeaturedActive.setVisibility(View.VISIBLE);
+                        break;
+
+                    case 1:
+                        mNavRecentActive  .setVisibility(View.VISIBLE);
+                        break;
+
+                    case 2:
+                        mNavSettingsActive.setVisibility(View.VISIBLE);
+                        break;
+                }
             }
         });
     }
 
     /**
-     * Setup tabs associated with view pager. Uses
-     * a custom widget for total customization.
-     *
-     * TODO: Use custom widget.
+     * Deselect all navigation bar items.
      */
-    private void setupViewPagerTabs() {
-        mActionBar = getActionBar();
+    private void deselectNav() {
 
-        if (mActionBar != null) {
-            mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        mNavFeaturedActive.setVisibility(View.GONE);
+        mNavRecentActive  .setVisibility(View.GONE);
+        mNavSettingsActive.setVisibility(View.GONE);
+    }
 
-            // Iterate over each tab.
-            for (int tabIndex = 0; tabIndex < mAdapter.getCount(); tabIndex++) {
+    //==============================================================================================
+    // Click Methods
+    //==============================================================================================
 
-                // Initialize a tab.
-                ActionBar.Tab tab = mActionBar.newTab();
+    /**
+     * Change active nav item, and
+     * switch to associated fragment.
+     *
+     * @param button Button clicked.
+     */
+    @OnClick({R.id.main_screen_nav_featured,
+              R.id.main_screen_nav_recent,
+              R.id.main_screen_nav_settings})
+    @SuppressWarnings("unused")
+    public void navClicked(Button button) {
 
-                // Configure it.
-                tab.setTag(tabIndex);
-                tab.setTabListener(this);
-                tab.setText(mAdapter.getPageTitle(tabIndex));
+        deselectNav();
 
-                // Add it.
-                mActionBar.addTab(tab);
-            }
+        switch (button.getId()) {
+
+            case R.id.main_screen_nav_featured:
+                mNavFeaturedActive.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.main_screen_nav_recent:
+                mNavRecentActive  .setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.main_screen_nav_settings:
+                mNavSettingsActive.setVisibility(View.VISIBLE);
+                break;
         }
+
+        // Sync with view pager when selected.
+        mPager.setCurrentItem(Integer.parseInt((String) button.getTag()));
     }
 }
