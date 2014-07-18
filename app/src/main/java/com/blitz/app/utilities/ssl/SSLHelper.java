3cabc407072ce85ee78fa04bce305b5faa0a9c70
mrkcsc <miguel@miguelgaeta.com>
2014-07-18 11:36:20 -0700

    Added an SSL helper utility class (now used in both REST/Comet api's).

---
+++ app/src/main/java/com/blitz/app/utilities/ssl/SSLHelper.java
@@ -0,0 +1,67 @@
+package com.blitz.app.utilities.ssl;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * Created by mrkcsc on 7/18/14.
+ */
+public class SSLHelper {
+
+    /**
+     * Creates a socket factory that does not
+     * validate and certificates.
+     *
+     * @return Socket factory, null on error.
+     */
+    @SuppressWarnings("unused")
+    private SSLSocketFactory createInsecureSSLSocketFactory() {
+
+        // Create trust manager that does not validate certs chains.
+        final TrustManager[] trustAllCerts = new TrustManager[] {
+
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException { }
+
+                    @Override
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException { }
+
+                    @Override
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+                }
+        };
+
+        SSLContext sslContext = null;
+
+        try {
+
+            // Install the all-trusting trust manager.
+            sslContext = SSLContext.getInstance("SSL");
+
+        } catch (NoSuchAlgorithmException ignored) { }
+
+        try {
+
+            if (sslContext != null) {
+                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            }
+        } catch (KeyManagementException ignored) { }
+
+
+        if (sslContext != null) {
+
+            // Create factory with all-trusting manager.
+            return sslContext.getSocketFactory();
+        }
+
+        // Error.
+        return null;
+    }
+}
