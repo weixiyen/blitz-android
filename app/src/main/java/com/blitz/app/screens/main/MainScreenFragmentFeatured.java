d0d3dc6339a92d4724c04ffa1d514594f5db4e5b
mrkcsc <miguel@miguelgaeta.com>
2014-07-20 05:19:17 -0700

    Queueing rest portion.

--- app/src/main/java/com/blitz/app/screens/main/MainScreenFragmentFeatured.java
+++ app/src/main/java/com/blitz/app/screens/main/MainScreenFragmentFeatured.java
@@ -1,39 +1,47 @@
 package com.blitz.app.screens.main;
 
+import com.blitz.app.R;
+import com.blitz.app.models.objects.ObjectModelPlay;
+import com.blitz.app.models.rest.RestAPIOperation;
 import com.blitz.app.utilities.android.BaseFragment;
+import com.blitz.app.utilities.logging.LogHelper;
+
+import butterknife.OnClick;
 
 /**
  * Created by mrkcsc on 7/14/14.
  */
 public class MainScreenFragmentFeatured extends BaseFragment {
 
-    //private ObjectModelPreferences mModelPreferences;
+    //==============================================================================================
+    // Member Variables
+    //==============================================================================================
+
+    private ObjectModelPlay mModelPlay;
 
     //==============================================================================================
     // Click Methods
     //==============================================================================================
 
-    /*
-    @OnClick(R.id.main_screen_play) @SuppressWarnings("unused")
+    @OnClick(R.id.main_featured_play) @SuppressWarnings("unused")
     public void main_screen_play() {
 
         if (RestAPIOperation.shouldThrottle()) {
             return;
         }
 
-        if (mModelPreferences == null) {
-            mModelPreferences = new ObjectModelPreferences();
+        if (mModelPlay == null) {
+            mModelPlay = new ObjectModelPlay();
         }
 
-        // Set desired registration fields.
-        mModelPreferences.TestCall(new RestAPIOperation(this) {
+        // Enter the queue.
+        mModelPlay.enterQueue(getActivity(), new ObjectModelPlay.EnterQueueCallback() {
 
             @Override
-            public void success() {
+            public void onEnterQueue() {
 
-                Log.e("Blitz", "Test");
+                LogHelper.log("In queue! - Show the dialog!");
             }
         });
     }
-    */
 }
