4022d324add035d5525fe0a770a01e8e800151fb
mrkcsc <miguel@miguelgaeta.com>
2014-07-20 02:08:04 -0700

    Updated preferences JSON object to include mode results.

--- app/src/main/java/com/blitz/app/models/rest_objects/JsonObjectPreference.java
+++ app/src/main/java/com/blitz/app/models/rest_objects/JsonObjectPreference.java
@@ -4,6 +4,9 @@ package com.blitz.app.models.rest_objects;
  * Created by Miguel Gaeta on 6/29/14.
  */
 public class JsonObjectPreference extends JsonObject {
-    public String current_week;
-    public String current_year;
+
+    public int current_week;
+    public int current_year;
+    public boolean queue_available;
+    public String current_active_queue;
 }
