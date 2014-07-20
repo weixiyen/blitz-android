9d4cc1ccf13d88b5a44d4d95602305516fdc1027
mrkcsc <miguel@miguelgaeta.com>
2014-07-20 02:57:49 -0700

    Join queue implemented (user wants to play!).

--- app/src/main/java/com/blitz/app/models/objects/ObjectModelPlay.java
+++ app/src/main/java/com/blitz/app/models/objects/ObjectModelPlay.java
@@ -2,29 +2,62 @@ package com.blitz.app.models.objects;
 
 import android.app.Activity;
 
-import com.blitz.app.utilities.logging.LogHelper;
-
 /**
  * Created by mrkcsc on 7/20/14.
  */
 public class ObjectModelPlay {
 
     //==============================================================================================
+    // Member Variables
+    //==============================================================================================
+
+    // Required models.
+    private ObjectModelPreferences mModelPreferences;
+    private ObjectModelQueue       mModelQueue;
+
+    //==============================================================================================
+    // Constructor
+    //==============================================================================================
+
+    /**
+     * Initialize models needed for this one.
+     */
+    public ObjectModelPlay() {
+        mModelPreferences = new ObjectModelPreferences();
+        mModelQueue       = new ObjectModelQueue();
+    }
+
+    //==============================================================================================
     // Public Methods
     //==============================================================================================
 
-    public void enterQueue(Activity activity, EnterQueueCallback callback) {
-
-        // Create a preferences object.
-        ObjectModelPreferences modelPreferences = new ObjectModelPreferences();
+    /**
+     * User wants to play, enter them into the queue.
+     *
+     * @param activity Activity for dialogs.
+     * @param callback Callback.
+     */
+    public void enterQueue(final Activity activity, final EnterQueueCallback callback) {
 
         // Now sync user preferences.
-        modelPreferences.Sync(activity, new ObjectModelPreferences.SyncCallback() {
+        mModelPreferences.Sync(activity, new ObjectModelPreferences.SyncCallback() {
 
             @Override
             public void onSync() {
 
-                LogHelper.log("Synced!");
+                // Set draft key to the active queue.
+                mModelQueue.setDraftKey(mModelPreferences.currentActiveQueue());
+
+                // Queue up for the draft!
+                mModelQueue.queueUp(activity, new ObjectModelQueue.QueueUpCallback() {
+
+                    @Override
+                    public void onQueueUp() {
+
+                        // Now in queue.
+                        callback.onEnterQueue();
+                    }
+                });
             }
         });
     }
