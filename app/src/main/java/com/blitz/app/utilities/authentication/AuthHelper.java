060e8d4f442d456bbbbbb423198878d4f275143f
mrkcsc <miguel@miguelgaeta.com>
2014-07-20 02:19:58 -0700

    Added a sign in check and a passed queue check to the authentication manager.

--- app/src/main/java/com/blitz/app/utilities/authentication/AuthHelper.java
+++ app/src/main/java/com/blitz/app/utilities/authentication/AuthHelper.java
@@ -1,7 +1,60 @@
 package com.blitz.app.utilities.authentication;
 
+import com.blitz.app.utilities.app.AppDataObject;
+
 /**
  * Created by mrkcsc on 7/20/14.
  */
 public class AuthHelper {
+
+    //==============================================================================================
+    // Member Variables
+    //==============================================================================================
+
+    // Instance object.
+    private static AuthHelper mInstance;
+
+    //==============================================================================================
+    // Public Methods
+    //==============================================================================================
+
+    /**
+     * Is user signed in.
+     *
+     * @return Yes/No.
+     */
+    public static boolean isSignedIn() {
+        return AppDataObject.userId.getString() != null;
+    }
+
+    /**
+     * Is user past queue screen.
+     *
+     * @return Yes/No.
+     */
+    public static boolean isPassedQueue() {
+        return AppDataObject.hasAccess.getBoolean();
+    }
+
+    //==============================================================================================
+    // Private Methods
+    //==============================================================================================
+
+    /**
+     * Fetch singleton.
+     *
+     * @return Singleton object.
+     */
+    private static AuthHelper instance() {
+
+        if (mInstance == null) {
+            synchronized (AuthHelper.class) {
+                if (mInstance == null) {
+                    mInstance = new AuthHelper();
+                }
+            }
+        }
+
+        return mInstance;
+    }
 }
