152f877c83f18a7ce72876d550f742f0d7f28f3e
mrkcsc <miguel@miguelgaeta.com>
2014-07-21 00:27:27 -0700

    Added remaining queue rest calls (DELETE/PUT).

--- app/src/main/java/com/blitz/app/models/rest/RestAPI.java
+++ app/src/main/java/com/blitz/app/models/rest/RestAPI.java
@@ -8,8 +8,11 @@ import com.blitz.app.models.rest_objects.JsonObjectUsers;
 
 import retrofit.Callback;
 import retrofit.http.Body;
+import retrofit.http.DELETE;
 import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.PUT;
+import retrofit.http.Path;
 
 /**
  * Created by Miguel Gaeta on 6/26/14.
@@ -22,29 +25,36 @@ public interface RestAPI {
 
     @GET("/preferences")
     void preferences(
-            Callback<JsonObjectPreference> callback);
+
+               Callback<JsonObjectPreference> callback);
 
     @POST("/code")
-    void code(
-            @Body JsonObjectCode.Body body,
+    void code(@Body JsonObjectCode.Body body,
 
-            Callback<JsonObjectCode> callback);
+               Callback<JsonObjectCode> callback);
 
     @POST("/users")
-    void users(
-            @Body JsonObjectUsers.Body body,
+    void users(@Body JsonObjectUsers.Body body,
 
-            Callback<JsonObjectUsers> callback);
+               Callback<JsonObjectUsers> callback);
 
     @POST("/queue")
-    void queue(
-            @Body JsonObjectQueue.Body body,
+    void queue(@Body JsonObjectQueue.Body body,
 
-            Callback<JsonObjectQueue> callback);
+               Callback<JsonObjectQueue> callback);
+
+    @DELETE("/queue/{draft_key}")
+    void queue(@Path("draft_key") String draftKey,
+
+               Callback<JsonObjectQueue> callback);
+
+    @PUT("/queue")
+    void queue(@Body JsonObjectQueue.BodyPUT body,
+
+               Callback<JsonObjectQueue> callback);
 
     @POST("/auth")
-    void auth(
-            @Body JsonObjectAuth.Body body,
+    void auth(@Body JsonObjectAuth.Body body,
 
-            Callback<JsonObjectAuth> callback);
+               Callback<JsonObjectAuth> callback);
 }
