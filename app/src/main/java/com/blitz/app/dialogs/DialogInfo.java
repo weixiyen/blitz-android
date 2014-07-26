40ad9cf32f740754a4b11570512bbcc9cbc73363
mrkcsc <miguel@miguelgaeta.com>
2014-07-25 18:18:34 -0700

    Added public interface for the generic info dialog.

--- app/src/main/java/com/blitz/app/dialogs/DialogInfo.java
+++ app/src/main/java/com/blitz/app/dialogs/DialogInfo.java
@@ -1,15 +1,95 @@
 package com.blitz.app.dialogs;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.blitz.app.R;
 import com.blitz.app.utilities.android.BaseDialog;
+
+import butterknife.InjectView;
 
 /**
  * Created by Miguel Gaeta on 6/4/14.
  */
 public class DialogInfo extends BaseDialog {
 
+    //==============================================================================================
+    // Member Variables
+    //==============================================================================================
+
+    @InjectView(R.id.dialog_info_text)       TextView mInfoText;
+    @InjectView(R.id.dialog_info_left_button)  Button mInfoLButton;
+    @InjectView(R.id.dialog_info_right_button) Button mInfoRButton;
+
+    //==============================================================================================
+    // Constructor
+    //==============================================================================================
+
     public DialogInfo(Activity activity) {
         super(activity);
+
+        // Hide all elements on load.
+        mInfoText   .setVisibility(View.GONE);
+        mInfoLButton.setVisibility(View.GONE);
+        mInfoRButton.setVisibility(View.GONE);
+    }
+
+    //==============================================================================================
+    // Public Methods
+    //==============================================================================================
+
+    /**
+     * Set info text of dialog.
+     *
+     * @param text Text.
+     */
+    @SuppressWarnings("unused")
+    public void setInfoText(String text) {
+        mInfoText.setText(text);
+        mInfoText.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Setup left button of info dialog.
+     *
+     * @param text Text.
+     * @param clickAction Click action.
+     */
+    @SuppressWarnings("unused")
+    public void setInfoLeftButton(String text, final Runnable clickAction) {
+        mInfoLButton.setText(text);
+        mInfoLButton.setVisibility(View.VISIBLE);
+        mInfoLButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (clickAction != null) {
+                    clickAction.run();
+                }
+            }
+        });
+    }
+
+    /**
+     * Setup right button of info dialog.
+     *
+     * @param text Text.
+     * @param clickAction Click action.
+     */
+    @SuppressWarnings("unused")
+    public void setInfoRightButton(String text, final Runnable clickAction) {
+        mInfoRButton.setText(text);
+        mInfoRButton.setVisibility(View.VISIBLE);
+        mInfoRButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (clickAction != null) {
+                    clickAction.run();
+                }
+            }
+        });
     }
 }
