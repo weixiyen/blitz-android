58965693102cf5d22580a3ba5763ce4525bf437c
mrkcsc <miguel@miguelgaeta.com>
2014-07-25 18:20:17 -0700

    Added comet draft callback functionality into the main screen.  Will allow user to join draft from any tab (callbacks are todo).

--- app/src/main/java/com/blitz/app/screens/main/MainScreen.java
+++ app/src/main/java/com/blitz/app/screens/main/MainScreen.java
@@ -6,8 +6,13 @@ import android.view.View;
 import android.widget.Button;
 
 import com.blitz.app.R;
+import com.blitz.app.dialogs.DialogInfo;
+import com.blitz.app.models.comet.CometAPICallback;
+import com.blitz.app.models.comet.CometAPIManager;
 import com.blitz.app.utilities.android.BaseActivity;
+import com.blitz.app.utilities.app.AppDataObject;
 import com.blitz.app.utilities.viewpager.ViewPagerDepthTransformer;
+import com.google.gson.JsonObject;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -29,6 +34,9 @@ public class MainScreen extends BaseActivity {
     @InjectView(R.id.main_screen_nav_recent_active)   View mNavRecentActive;
     @InjectView(R.id.main_screen_nav_settings_active) View mNavSettingsActive;
 
+    // Info dialog.
+    private DialogInfo mDialogInfo;
+
     //==============================================================================================
     // Overwritten Methods
     //==============================================================================================
@@ -44,6 +52,9 @@ public class MainScreen extends BaseActivity {
 
         // Setup view pager.
         setupViewPager();
+
+        // Setup callbacks.
+        setupDraftCallbacks();
     }
 
     /**
@@ -62,8 +73,106 @@ public class MainScreen extends BaseActivity {
     }
 
     //==============================================================================================
+    // Public Methods
+    //==============================================================================================
+
+    /**
+     * Show a confirmation dialog for the draft.
+     */
+    public void showConfirmDraftDialog() {
+
+        // Initialize dialog.
+        if (mDialogInfo == null) {
+            mDialogInfo = new DialogInfo(this);
+
+            // Configure the dialog.
+            mDialogInfo.setInfoText("Match Found! Join the match!");
+            mDialogInfo.setInfoLeftButton("Cancel", new Runnable() {
+
+                @Override
+                public void run() {
+
+                    // TODO: Cancel the queue.
+                }
+            });
+            mDialogInfo.setInfoRightButton("Join!", new Runnable() {
+
+                @Override
+                public void run() {
+
+                    // TODO: Join the draft.
+                }
+            });
+        }
+
+        mDialogInfo.show(true);
+    }
+
+    /**
+     * Hide the confirmation dialog for the draft.
+     */
+    public void hideConfirmDraftDialog() {
+
+        // Hide if needed.
+        if (mDialogInfo != null) {
+            mDialogInfo.hide(null);
+        }
+    }
+
+    //==============================================================================================
     // Private Methods
     //==============================================================================================
+
+    /**
+     * Setup callbacks for the draft
+     * powered by the comet manager.
+     */
+    private void setupDraftCallbacks() {
+
+        // Fetch comet channel for this user.
+        String userCometChannel = "user:" + AppDataObject.userId.getString();
+
+        // Subscribe to channel, set callback.
+        CometAPIManager
+
+                // Subscribe to user channel.
+                .subscribeToChannel(userCometChannel)
+
+                // Set callback action.
+                .addCallback(this, new CometAPICallback<MainScreen>() {
+
+                    @Override
+                    public void messageReceived(MainScreen receivingClass, JsonObject message) {
+
+                        receivingClass.handleDraftAction(receivingClass, message);
+                    }
+                }, "draftUserCallbackMainScreen");
+    }
+
+    /**
+     * Handle a draft callback action.  Either
+     * show or hide a confirmation dialog, or
+     * simply enter the draft.
+     *
+     * @param receivingClass Instance of this activity.
+     * @param message Json message sent.
+     */
+    private void handleDraftAction(MainScreen receivingClass, JsonObject message) {
+
+        // Fetch sent action.
+        String action = message.get("action").getAsString();
+
+        if (action.equals("confirm_draft")) {
+
+            // Present confirmation dialog to user.
+            receivingClass.showConfirmDraftDialog();
+
+        } else if (action.equals("left_queue")) {
+
+            // Dismiss dialog.
+            receivingClass.hideConfirmDraftDialog();
+        }
+    }
 
     /**
      * Create and setup the viewpager and associated
