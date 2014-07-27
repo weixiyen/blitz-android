ce81a0fc43faf437bef8bcaf51eb986703175ad0
mrkcsc <miguel@miguelgaeta.com>
2014-07-27 01:13:00 -0700

    Added a view model class for features fragment.  Actual fragment now only defines views and user interactions.

---
+++ app/src/main/java/com/blitz/app/models/views/ViewModelMainFeatured.java
@@ -0,0 +1,276 @@
+package com.blitz.app.models.views;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blitz.app.models.comet.CometAPICallback;
+import com.blitz.app.models.comet.CometAPIManager;
+import com.blitz.app.models.objects.ObjectModelQueue;
+import com.blitz.app.screens.main.MainScreenFragmentFeatured;
+import com.blitz.app.utilities.app.AppDataObject;
+import com.google.gson.JsonObject;
+
+/**
+ * Created by Miguel on 7/26/2014.
+ */
+public class ViewModelMainFeatured extends ViewModel {
+
+    //==============================================================================================
+    // Member Variables
+    //==============================================================================================
+
+    // View containers.
+    private View mTimelineContainer;
+    private View mQueuedContainer;
+    private TextView mTimerTextView;
+
+    // Queue timer variables.
+    private int      mSecondsInQueue;
+    private Handler  mSecondsInQueueHandler;
+    private Runnable mSecondsInQueueRunnable;
+
+    // Object model.
+    private ObjectModelQueue mModelQueue;
+
+    //==============================================================================================
+    // Overwritten Methods
+    //==============================================================================================
+
+    /**
+     * Restore model variables and re-initialize
+     * the UI for the screen.
+     *
+     * @param savedInstanceState Contains state information.
+     */
+    @Override
+    public void restoreInstanceState(Bundle savedInstanceState) {
+
+        // TODO: Restore state.
+
+        // Initialize container state.
+        showQueueContainer(null);
+
+        // Setup comet.
+        setupCometCallbacks();
+    }
+
+    /**
+     * Clean up the UI and persist model state.
+     *
+     * @param savedInstanceState State information bundle.
+     *
+     * @return Bundle with added state.
+     */
+    @Override
+    public Bundle saveInstanceState(Bundle savedInstanceState) {
+
+        // TODO: Save state.
+
+        // Stop timer.
+        stopQueueTimer();
+
+        return savedInstanceState;
+    }
+
+    //==============================================================================================
+    // Public Methods
+    //==============================================================================================
+
+    /**
+     * Setter for various views in the UI that
+     * we will be operating on.
+     *
+     * @param timelineContainer Timeline container, shows betting schedule.
+     * @param queuedContainer Queue container, shows the queue timer.
+     * @param timerTextView Time text view.
+     */
+    public void setViews(View timelineContainer, View queuedContainer, TextView timerTextView) {
+
+        mTimelineContainer = timelineContainer;
+        mQueuedContainer   = queuedContainer;
+        mTimerTextView     = timerTextView;
+    }
+
+    /**
+     * Join the draft queue.
+     */
+    public void queueUp() {
+
+        // Enter the queue.
+        getModelQueue().queueUp(mActivity, new ObjectModelQueue.QueueUpCallback() {
+
+            @Override
+            public void onQueueUp() {
+
+                showQueueContainer(true);
+            }
+        });
+    }
+
+    /**
+     * Leave the draft queue.
+     */
+    public void leaveQueue() {
+
+        // Leave the queue.
+        getModelQueue().leaveQueue(mActivity, null);
+    }
+
+    //==============================================================================================
+    // Private Methods
+    //==============================================================================================
+
+    /**
+     * Show the queue container to either hidden or
+     * visible.
+     *
+     * @param showQueueContainer Boolean toggle, if null uses internal
+     *                           state synchronize UI with that state.
+     */
+    private void showQueueContainer(Boolean showQueueContainer) {
+
+        if (showQueueContainer == null) {
+            showQueueContainer = mSecondsInQueue > 0;
+        }
+
+        if (showQueueContainer) {
+            showContainer(mTimelineContainer, mQueuedContainer);
+            startQueueTimer(mTimerTextView);
+        } else {
+            showContainer(mQueuedContainer, mTimelineContainer);
+            stopQueueTimer();
+        }
+    }
+
+    /**
+     * Listen on user channel for callbacks
+     * related to joining the game.
+     */
+    private void setupCometCallbacks() {
+
+        // Fetch comet channel for this user.
+        String userCometChannel = "user:" + AppDataObject.userId.getString();
+
+        // Subscribe to channel, set callback.
+        CometAPIManager
+
+                // Subscribe to user channel.
+                .subscribeToChannel(userCometChannel)
+
+                        // Set callback action.
+                .addCallback(MainScreenFragmentFeatured.class, new CometAPICallback<MainScreenFragmentFeatured>() {
+
+                    @Override
+                    public void messageReceived(MainScreenFragmentFeatured receivingClass, JsonObject message) {
+
+                        // Handle the action.
+                        receivingClass.getViewModel().handleDraftAction(receivingClass, message);
+                    }
+                }, "draftUserCallback");
+    }
+
+    /**
+     * Handle a draft callback action.
+     *
+     * @param receivingClass Instance of this activity.
+     * @param message Json message sent.
+     */
+    private void handleDraftAction(MainScreenFragmentFeatured receivingClass, JsonObject message) {
+
+        // Fetch sent action.
+        String action = message.get("action").getAsString();
+
+        // If left the queue.
+        if (action.equals("left_queue")) {
+
+            receivingClass.getViewModel().showQueueContainer(false);
+        }
+    }
+
+    /**
+     * Start the queue timer and increment
+     * it every second.
+     */
+    private void startQueueTimer(final TextView timerTextView) {
+
+        if (mSecondsInQueueHandler ==  null) {
+            mSecondsInQueueHandler = new Handler();
+        }
+
+        if (mSecondsInQueueRunnable == null) {
+            mSecondsInQueueRunnable = new Runnable() {
+
+                @Override
+                public void run() {
+
+                    mSecondsInQueue++;
+                    mSecondsInQueueHandler.postDelayed(mSecondsInQueueRunnable, 1000);
+
+                    // Update the timer text view.
+                    timerTextView.setText(String.format("%02d:%02d",
+                            mSecondsInQueue / 100, mSecondsInQueue % 100));
+                }
+            };
+        }
+
+        // Start the timer.
+        mSecondsInQueueHandler.post(mSecondsInQueueRunnable);
+        mSecondsInQueue = -1;
+    }
+
+    /**
+     * Stop timer and reset seconds
+     * in the queue.
+     */
+    private void stopQueueTimer() {
+
+        if (mSecondsInQueueHandler  != null &&
+            mSecondsInQueueRunnable != null) {
+
+            // Stop the timer.
+            mSecondsInQueueHandler.removeCallbacks(mSecondsInQueueRunnable);
+        }
+    }
+
+    /**
+     * Show a container with animation.
+     *
+     * @param containerFrom Animating from.
+     * @param containerTo Animating to.
+     */
+    private void showContainer(final View containerFrom, final View containerTo) {
+
+        // First fade out the from container.
+        containerFrom.animate().alpha(0.0f).setDuration(250).withEndAction(new Runnable() {
+
+            @Override
+            public void run() {
+
+                // Then remove it from the layout.
+                containerFrom.setVisibility(View.GONE);
+
+                // Then fade in the to container.
+                containerTo.setVisibility(View.VISIBLE);
+                containerTo.setAlpha(0.0f);
+                containerTo.animate().alpha(1.0f).setDuration(250);
+            }
+        });
+    }
+
+    /**
+     * Fetch queue model instance.
+     *
+     * @return Queue model instance.
+     */
+    private ObjectModelQueue getModelQueue() {
+
+        // Lazy load the model.
+        if (mModelQueue == null) {
+            mModelQueue = new ObjectModelQueue();
+        }
+
+        return mModelQueue;
+    }
+}
--- app/src/main/java/com/blitz/app/screens/main/MainScreenFragmentFeatured.java
+++ app/src/main/java/com/blitz/app/screens/main/MainScreenFragmentFeatured.java
@@ -1,18 +1,13 @@
 package com.blitz.app.screens.main;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
 import com.blitz.app.R;
-import com.blitz.app.models.comet.CometAPICallback;
-import com.blitz.app.models.comet.CometAPIManager;
-import com.blitz.app.models.objects.ObjectModelQueue;
 import com.blitz.app.models.rest.RestAPIOperation;
+import com.blitz.app.models.views.ViewModelMainFeatured;
 import com.blitz.app.utilities.android.BaseFragment;
-import com.blitz.app.utilities.app.AppDataObject;
-import com.google.gson.JsonObject;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -33,13 +28,8 @@ public class MainScreenFragmentFeatured extends BaseFragment {
     // Queue timer.
     @InjectView(R.id.main_featured_queued_timer) TextView mQueuedTimerTextView;
 
-    // View model.
-    private ObjectModelQueue mModelQueue;
-
-    // Queue timer variables.
-    private int      mSecondsInQueue;
-    private Handler  mSecondsInQueueHandler;
-    private Runnable mSecondsInQueueRunnable;
+    // Associated view model.
+    private ViewModelMainFeatured mViewModel;
 
     //==============================================================================================
     // Overwritten Methods
@@ -54,179 +44,66 @@ public class MainScreenFragmentFeatured extends BaseFragment {
     protected void onCreateView(Bundle savedInstanceState) {
         super.onCreateView(savedInstanceState);
 
-        // Lazy load the model.
-        if (mModelQueue == null) {
-            mModelQueue = new ObjectModelQueue();
-        }
+        // Initialize view model.
+        mViewModel = new ViewModelMainFeatured();
+        mViewModel.setActivity(getActivity());
+        mViewModel.setViews(mTimelineContainer, mQueuedContainer, mQueuedTimerTextView);
 
-        // Setup comet.
-        setupCometCallbacks();
+        // Restore state.
+        mViewModel.restoreInstanceState(savedInstanceState);
     }
 
     /**
-     * Cleanup on fragment pause.
+     * Save this screen fragments state.
+     *
+     * @param outState Outbound state bundle.
      */
     @Override
-    public void onPause () {
-        super.onPause();
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-        // Stop timer if running.
-        stopQueueTimer();
-    }
-
-    //==============================================================================================
-    // Private Methods
-    //==============================================================================================
-
-    /**
-     * Start the queue timer and increment
-     * it every second.
-     */
-    private void startQueueTimer() {
-
-        if (mSecondsInQueueHandler ==  null) {
-            mSecondsInQueueHandler = new Handler();
-        }
-
-        if (mSecondsInQueueRunnable == null) {
-            mSecondsInQueueRunnable = new Runnable() {
-
-                @Override
-                public void run() {
-
-                    mSecondsInQueue++;
-                    mSecondsInQueueHandler.postDelayed(mSecondsInQueueRunnable, 1000);
-
-                    // Update the timer text view.
-                    mQueuedTimerTextView.setText(String.format("%02d:%02d",
-                            mSecondsInQueue / 100, mSecondsInQueue % 100));
-                }
-            };
-        }
-
-        // Start the timer.
-        mSecondsInQueueHandler.post(mSecondsInQueueRunnable);
-        mSecondsInQueue = -1;
+        // Save state.
+        mViewModel.saveInstanceState(outState);
     }
 
     /**
-     * Stop timer and reset seconds
-     * in the queue.
-     */
-    private void stopQueueTimer() {
-
-        if (mSecondsInQueueHandler  != null &&
-            mSecondsInQueueRunnable != null) {
-
-            // Stop the timer.
-            mSecondsInQueueHandler.removeCallbacks(mSecondsInQueueRunnable);
-        }
-    }
-
-    /**
-     * Show a container with animation.
+     * Fetch the view model.
      *
-     * @param containerFrom Animating from.
-     * @param containerTo Animating to.
+     * @return View model.
      */
-    private void showContainer(final View containerFrom, final View containerTo) {
-
-        // First fade out the from container.
-        containerFrom.animate().alpha(0.0f).setDuration(250).withEndAction(new Runnable() {
-
-            @Override
-            public void run() {
-
-                // Then remove it from the layout.
-                containerFrom.setVisibility(View.GONE);
-
-                // Then fade in the to container.
-                containerTo.setVisibility(View.VISIBLE);
-                containerTo.setAlpha(0.0f);
-                containerTo.animate().alpha(1.0f).setDuration(250);
-            }
-        });
-    }
-
-    /**
-     * Listen on user channel for callbacks
-     * related to joining the game.
-     */
-    private void setupCometCallbacks() {
-
-        // Fetch comet channel for this user.
-        String userCometChannel = "user:" + AppDataObject.userId.getString();
-
-        // Subscribe to channel, set callback.
-        CometAPIManager
-
-                // Subscribe to user channel.
-                .subscribeToChannel(userCometChannel)
-
-                // Set callback action.
-                .addCallback(this, new CometAPICallback<MainScreenFragmentFeatured>() {
-
-                    @Override
-                    public void messageReceived(MainScreenFragmentFeatured receivingClass, JsonObject message) {
-
-                        // Handle the action.
-                        receivingClass.handleDraftAction(receivingClass, message);
-                    }
-                }, "draftUserCallback");
-    }
-
-    /**
-     * Handle a draft callback action.
-     *
-     * @param receivingClass Instance of this activity.
-     * @param message Json message sent.
-     */
-    private void handleDraftAction(MainScreenFragmentFeatured receivingClass, JsonObject message) {
-
-        // Fetch sent action.
-        String action = message.get("action").getAsString();
-
-        // If left the queue.
-        if (action.equals("left_queue")) {
-
-            // Hide the timeline UI.
-            receivingClass.showContainer(mQueuedContainer, mTimelineContainer);
-
-            // Stop timer.
-            receivingClass.stopQueueTimer();
-        }
+    public ViewModelMainFeatured getViewModel() {
+        return mViewModel;
     }
 
     //==============================================================================================
     // Click Methods
     //==============================================================================================
 
+    /**
+     * Join the draft queue!
+     */
     @OnClick(R.id.main_featured_play) @SuppressWarnings("unused")
     public void main_featured_play() {
 
-        if (RestAPIOperation.shouldThrottle()) { return; }
+        if (RestAPIOperation.shouldThrottle()) {
+            return;
+        }
 
         // Enter the queue.
-        mModelQueue.queueUp(getActivity(), new ObjectModelQueue.QueueUpCallback() {
-
-            @Override
-            public void onQueueUp() {
-
-                // Show the queue UI.
-                showContainer(mTimelineContainer, mQueuedContainer);
-
-                // Start timer.
-                startQueueTimer();
-            }
-        });
+        mViewModel.queueUp();
     }
 
+    /**
+     * Leave the draft queue.
+     */
     @OnClick(R.id.main_featured_cancel) @SuppressWarnings("unused")
     public void main_featured_cancel() {
 
-        if (RestAPIOperation.shouldThrottle()) { return; }
+        if (RestAPIOperation.shouldThrottle()) {
+            return;
+        }
 
         // Leave the queue.
-        mModelQueue.leaveQueue(getActivity(), null);
+        mViewModel.leaveQueue();
     }
 }
