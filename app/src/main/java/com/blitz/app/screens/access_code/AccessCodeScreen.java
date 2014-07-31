330047760bcf8dfbaaf2600b532ccf2052e4b824
mrkcsc <miguel@miguelgaeta.com>
2014-07-30 22:38:25 -0700

    Animations for the access code screen.

--- app/src/main/java/com/blitz/app/screens/access_code/AccessCodeScreen.java
+++ app/src/main/java/com/blitz/app/screens/access_code/AccessCodeScreen.java
@@ -2,6 +2,7 @@ package com.blitz.app.screens.access_code;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.blitz.app.R;
@@ -9,6 +10,9 @@ import com.blitz.app.models.objects.ObjectModelCode;
 import com.blitz.app.models.rest.RestAPIOperation;
 import com.blitz.app.screens.splash.SplashScreen;
 import com.blitz.app.utilities.android.BaseActivity;
+import com.blitz.app.utilities.animations.AnimHelper;
+import com.blitz.app.utilities.animations.AnimHelperPresets;
+import com.blitz.app.utilities.animations.AnimHelperView;
 import com.blitz.app.utilities.app.AppDataObject;
 
 import butterknife.InjectView;
@@ -23,7 +27,13 @@ public class AccessCodeScreen extends BaseActivity {
     // Member Variables
     //==============================================================================================
 
-    @InjectView(R.id.access_code_screen_code) EditText mCode;
+    @InjectView(R.id.access_code_continue_with_code) View mContinue;
+    @InjectView(R.id.access_code_code)           EditText mCode;
+    @InjectView(R.id.access_code_player)             View mPlayer;
+    @InjectView(R.id.access_code_header)             View mHeader;
+
+    private AnimHelper animsFromTop;
+    private AnimHelper animsFromRight;
 
     private ObjectModelCode mObjectModelCode;
 
@@ -42,13 +52,41 @@ public class AccessCodeScreen extends BaseActivity {
 
         // This screen uses a slide animation.
         setCustomTransitions(CustomTransition.T_SLIDE_HORIZONTAL);
+
+        // Text and call to action.
+        animsFromTop = AnimHelper.from(this, 25, 5)
+                .addHelperView(AnimHelperView.from(mHeader, AnimHelperPresets.SLIDE_DOWN))
+                .addHelperView(AnimHelperView.from(mCode, AnimHelperPresets.SLIDE_DOWN))
+                .addHelperView(AnimHelperView.from(mContinue, AnimHelperPresets.SLIDE_UP));
+
+        // Queue container text.
+        animsFromRight = AnimHelper.from(this, 100, 30)
+                .addHelperView(AnimHelperView.from(mPlayer, AnimHelperPresets.SLIDE_LEFT));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Enable.
+        animsFromTop.enable();
+        animsFromRight.enable();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Disable.
+        animsFromTop.disable();
+        animsFromRight.disable();
     }
 
     //==============================================================================================
     // Click Methods
     //==============================================================================================
 
-    @OnClick(R.id.access_code_screen_continue_with_code) @SuppressWarnings("unused")
+    @OnClick(R.id.access_code_continue_with_code) @SuppressWarnings("unused")
     public void continueWithCode() {
 
         if (RestAPIOperation.shouldThrottle()) {
