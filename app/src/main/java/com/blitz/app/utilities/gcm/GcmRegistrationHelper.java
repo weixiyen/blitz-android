416635b08d5e9071305b27c8bffdfece61836ff1
mrkcsc <miguel@miguelgaeta.com>
2014-08-06 01:58:00 -0700

    Added sections and a method for retrieving application id

--- app/src/main/java/com/blitz/app/utilities/gcm/GcmRegistrationHelper.java
+++ app/src/main/java/com/blitz/app/utilities/gcm/GcmRegistrationHelper.java
@@ -1,6 +1,9 @@
 package com.blitz.app.utilities.gcm;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -10,15 +13,24 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
  */
 public class GcmRegistrationHelper {
 
+    //==============================================================================================
+    // Member Variables
+    //==============================================================================================
+
     // Request tag for asking user to obtain google play services.
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    //==============================================================================================
+    // Private Methods
+    //==============================================================================================
 
     /**
      * Check the device to make sure it has the Google Play Services APK. If
      * it doesn't, display a dialog that allows users to download the APK from
      * the Google Play Store or enable it in the device's system settings.
      */
-    private boolean checkPlayServices(Activity activity) {
+    @SuppressWarnings("unused")
+    private static boolean checkPlayServices(Activity activity) {
 
         // Check to see if google play services available on this device.
         int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
@@ -37,5 +49,23 @@ public class GcmRegistrationHelper {
         }
 
         return true;
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    @SuppressWarnings("unused")
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+
+            return packageInfo.versionCode;
+
+        } catch (PackageManager.NameNotFoundException e) {
+
+            // Should never happen.
+            throw new RuntimeException("Could not get package name: " + e);
+        }
     }
 }
