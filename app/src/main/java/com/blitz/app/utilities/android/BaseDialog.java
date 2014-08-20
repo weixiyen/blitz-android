a11abac176c586f7c830bd57601cff81fbe9106a
mrkcsc <miguel@miguelgaeta.com>
2014-08-19 21:21:13 -0700

    Guarded illegal state exception.

--- app/src/main/java/com/blitz/app/utilities/android/BaseDialog.java
+++ app/src/main/java/com/blitz/app/utilities/android/BaseDialog.java
@@ -208,6 +208,7 @@ public class BaseDialog {
                     }
 
                 } catch (IllegalArgumentException ignored) { }
+                  catch (IllegalStateException    ignored) { }
 
                 // Execute callback.
                 if (mHideListener != null) {
