faf96fc4146ccc2c5b317a2e508c27a258fbe996
mrkcsc <miguel@miguelgaeta.com>
2014-08-24 00:58:51 -0700

    Added some basic UI for the draft screen.

--- app/src/main/java/com/blitz/app/screens/draft/DraftScreen.java
+++ app/src/main/java/com/blitz/app/screens/draft/DraftScreen.java
@@ -1,9 +1,46 @@
 package com.blitz.app.screens.draft;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.blitz.app.R;
 import com.blitz.app.utilities.android.BaseActivity;
+
+import butterknife.InjectView;
 
 /**
  * Created by mrkcsc on 7/27/14.
  */
 public class DraftScreen extends BaseActivity {
+
+    @InjectView(R.id.draft_header) TextView mDraftHeader;
+
+    /**
+     * When screen is created.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    /**
+     * Runs when activity has been
+     * presented to the user.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    /**
+     * Run custom transitions if needed, also
+     * start timer to detect entering background.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
 }
