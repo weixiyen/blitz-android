c6c896678deeb3185ad7a51a8d9aec4fca57dd1a
Nate Black <spiffy1911@gmail.com>
2014-09-05 20:01:08 -0700

    added UIObserver

---
+++ app/src/main/java/com/blitz/app/utilities/ui/UIObserver.java
@@ -0,0 +1,38 @@
+package com.blitz.app.utilities.ui;
+
+import android.widget.TextView;
+
+import rx.Observer;
+
+/**
+ * Created by Nate on 8/25/14.
+ */
+public class UIObserver {
+
+    /**
+     * Makes an Observer that sets the TextView's value to the observed value.
+     */
+    public static<T> Observer<T> textField(final TextView textView) {
+        // TODO should create a weak reference to textView?
+        return new Observer<T>() {
+            @Override
+            public void onCompleted() {
+
+                throw new IllegalStateException("TextView observables shouldn't ever finish");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+                throw new RuntimeException("Unexpected error");
+            }
+
+            @Override
+            public void onNext(T t) {
+
+                textView.setText(t == null ? null : t.toString());
+            }
+        };
+
+    }
+}
