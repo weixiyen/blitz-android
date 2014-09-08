27accd641797384c687c9985e1ece2ca4b897c2b
mrkcsc <miguel@miguelgaeta.com>
2014-09-07 21:15:28 -0700

    The draft model now has skeleton calls to get active drafts and get drafts for user.  They work but do not parse the JSON result yet.

--- app/src/main/java/com/blitz/app/object_models/ObjectModelDraft.java
+++ app/src/main/java/com/blitz/app/object_models/ObjectModelDraft.java
@@ -7,6 +7,9 @@ import com.blitz.app.utilities.rest.RestAPIClient;
 import com.blitz.app.utilities.rest.RestAPIObject;
 import com.blitz.app.utilities.rest.RestAPIOperation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mrkcsc on 7/27/14. Copyright 2014 Blitz Studios
  */
@@ -60,6 +63,111 @@ public class ObjectModelDraft {
         // Make api call.
         RestAPIClient.getAPI().draft_get
                 (mDraftId, RestAPICallback.create(operation));
+    }
+
+    /**
+     * Fetch list of the users current active drafts.  It is a serious
+     * error if this method fails so if it does we must log out the user.
+     *
+     * @param activity Activity fo loading/error dialogs.
+     * @param userId User id.
+     * @param callback Success callback, provides the list of drafts.
+     */
+    @SuppressWarnings("unused")
+    public static void fetchActiveDraftsForUser(Activity activity, String userId,
+                                                final DraftsCallback callback) {
+
+        RestAPIOperation operation = new RestAPIOperation(activity) {
+
+            @Override
+            public void success(RestAPIObject restAPIObject) {
+
+                // TODO: Parse and create drafts.
+
+                if (callback != null) {
+                    callback.onSuccess(null);
+                }
+            }
+
+            /**
+             * Force log out the user on failure.
+             */
+            @Override
+            public void failure(boolean logout) {
+                super.failure(true);
+            }
+        };
+
+        // Filter by currently drafting.
+        String filter = "{\"status\": \"drafting\", \"model\": \"heads_up_draft\"}";
+
+        // Sort by most recent.
+        String orderBy = "{\"created\": \"ASC\"}";
+
+        RestAPIClient.getAPI().drafts_get(getKeys(userId), null, "users",
+                filter, orderBy, null, RestAPICallback.create(operation));
+    }
+
+    @SuppressWarnings("unused")
+    public static void fetchDraftsForUser(Activity activity, String userId,
+                                          Integer week,
+                                          Integer year,
+                                          Integer limit,
+                                          final DraftsCallback callback) {
+
+        RestAPIOperation operation = new RestAPIOperation(activity) {
+
+            @Override
+            public void success(RestAPIObject restAPIObject) {
+
+                // TODO: Parse and create drafts.
+
+                if (callback != null) {
+                    callback.onSuccess(null);
+                }
+            }
+        };
+
+        // Create filter for current time frame.
+        String filter = "{\"week\": " + week + ", \"year\": " +
+                year + ", \"model\": \"heads_up_draft\"}";
+
+        // Order by completed and created descending.
+        String orderBy = "{\"completed\": \"DESC\", \"created\": \"DESC\"}";
+
+        RestAPIClient.getAPI().drafts_get(getKeys(userId), null, "users",
+                filter, orderBy, limit, RestAPICallback.create(operation));
+    }
+
+    // endregion
+
+    // region Private Methods
+    // =============================================================================================
+
+    /**
+     * Fetch keys for draft.
+     *
+     * @param userId User id.
+     *
+     * @return List of keys.
+     */
+    private static ArrayList<String> getKeys(String userId) {
+
+        ArrayList<String> keys = new ArrayList<String>();
+
+        keys.add(userId);
+
+        return keys;
+    }
+
+    // endregion
+
+    // region Callbacks
+    // =============================================================================================
+
+    public interface DraftsCallback {
+
+        public void onSuccess(List<ObjectModelDraft> drafts);
     }
 
     // endregion
