f0ee0dbd816dcadfbdc6ca4c2cb292797676a0e0
mrkcsc <miguel@miguelgaeta.com>
2014-09-21 19:20:19 -0700

    Added member variables and protected methods from REST operation.

--- app/src/main/java/com/blitz/app/utilities/rest/RestAPICallbackCombined.java
+++ app/src/main/java/com/blitz/app/utilities/rest/RestAPICallbackCombined.java
@@ -1,5 +1,13 @@
 package com.blitz.app.utilities.rest;
 
+import android.app.Activity;
+import android.os.Handler;
+
+import com.blitz.app.dialogs.error.DialogError;
+import com.blitz.app.dialogs.loading.DialogLoading;
+
+import java.util.Date;
+
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -13,15 +21,34 @@ public abstract class RestAPICallbackCombined<T> implements Callback<T> {
     // Overwritten Methods
     //==============================================================================================
 
-    // The model operation object.
-    // TODO: Revise private RestAPIOperation mOperation;
+    // Parent activity.
+    private Activity mActivity;
+
+    // Loading dialog.
+    private DialogLoading mDialogLoading;
+    private DialogError mDialogError;
+
+    private static boolean mOperationThrottle;
+    private static Handler mOperationThrottleHandler;
+    private static Runnable mOperationThrottleRunnable;
+
+    // Operation time.
+    private long mOperationTimeMilliseconds;
+    private Date mOperationTimeStart;
+    private Date mOperationTimeEnd;
 
     // Is this an auth call.
     private boolean mIsAuthentication;
 
-    //==============================================================================================
-    // Constructor
-    //==============================================================================================
+    // region Abstract Methods
+    // =============================================================================================
+
+    public abstract void success(T jsonObject);
+
+    // endregion
+
+    // region Constructor
+    // =============================================================================================
 
     /**
      * Assign our member variables.
@@ -35,35 +62,10 @@ public abstract class RestAPICallbackCombined<T> implements Callback<T> {
         // TODO: Revise mOperation.start();
     }
 
-    // region Public Methods
-    // =============================================================================================
-
-    /**
-     * Is this an authentication callback.
-     *
-     * @param isAuthentication Is this an authentication call.
-     */
-    @SuppressWarnings("unused")
-    public void setIsAuthentication(boolean isAuthentication) {
-        mIsAuthentication = isAuthentication;
-    }
-
-    public void failure(Response response, boolean networkError) {
-
-    }
-
     // endregion
 
-    // region Abstract Methods
+    // region Overwritten Methods
     // =============================================================================================
-
-    public abstract void success(T jsonObject);
-
-    // endregion
-
-    //==============================================================================================
-    // Overwritten Methods
-    //==============================================================================================
 
     /**
      * Successful REST call is made.
@@ -101,4 +103,95 @@ public abstract class RestAPICallbackCombined<T> implements Callback<T> {
         // Finish with error.
         // TODO: Revise mOperation.finish(null, retrofitError);
     }
+
+    // endregion
+
+    // region Public Methods
+    // =============================================================================================
+
+    /**
+     * Is this an authentication callback.
+     *
+     * @param isAuthentication Is this an authentication call.
+     */
+    @SuppressWarnings("unused")
+    public void setIsAuthentication(boolean isAuthentication) {
+        mIsAuthentication = isAuthentication;
+    }
+
+    @SuppressWarnings("unused")
+    public void failure(Response response, boolean networkError) {
+
+    }
+
+    // endregion
+
+    // region Protected Methods
+    // =============================================================================================
+
+    /**
+     * Fetch how long it took to
+     * run this operation.
+     *
+     * @return Time in milliseconds.
+     */
+    @SuppressWarnings("unused")
+    protected Long getOperationTime() {
+
+        return mOperationTimeMilliseconds;
+    }
+
+    /**
+     * Fetch time the operation started.
+     *
+     * @return Operation start time.
+     */
+    @SuppressWarnings("unused")
+    protected Date getOperationTimeStart() {
+
+        return mOperationTimeStart;
+    }
+
+    /**
+     * Fetch time operation ended.
+     *
+     * @return Operation end time.
+     */
+    @SuppressWarnings("unused")
+    protected Date getOperationTimeEnd() {
+
+        return mOperationTimeEnd;
+    }
+
+    /**
+     * Lazy load the error dialog.
+     *
+     * @return Error dialog.
+     */
+    @SuppressWarnings("unused")
+    protected DialogError getErrorDialog() {
+
+        if (mDialogError == null && mActivity != null) {
+            mDialogError = new DialogError(mActivity);
+        }
+
+        return mDialogError;
+    }
+
+    /**
+     * Lazy load the loading dialog.
+     *
+     * @return Loading dialog.
+     */
+    @SuppressWarnings("unused")
+    protected DialogLoading getLoadingDialog() {
+
+        if (mDialogLoading == null && mActivity != null) {
+            mDialogLoading = new DialogLoading(mActivity);
+        }
+
+        return mDialogLoading;
+    }
+
+    // endregion
 }
