91b2b8aafc9face3efd8ea4186513cac051ffa8d
mrkcsc <miguel@miguelgaeta.com>
2014-09-21 19:08:24 -0700

    Added comments.

--- app/src/main/java/com/blitz/app/utilities/rest/RestAPIResult.java
+++ app/src/main/java/com/blitz/app/utilities/rest/RestAPIResult.java
@@ -1,13 +1,35 @@
 package com.blitz.app.utilities.rest;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 /**
- * Created by Nate on 9/18/14.
+ * Created by Nate on 9/18/14. Copyright 2014 Blitz Studios
  */
 public class RestAPIResult<T> {
 
-    private List<T> results;
+    // region Member Variables
+    // =============================================================================================
 
-    public List<T> getResults() { return results; }
+    // Results field.
+    @SerializedName("results") @SuppressWarnings("unused") private List<T> mResults;
+
+    // endregion
+
+    // region Public Methods
+    // =============================================================================================
+
+    /**
+     * Fetch results object, some list of
+     * rest object models.
+     *
+     * @return List of type.
+     */
+    public List<T> getResults() {
+
+        return mResults;
+    }
+
+    // endregion
 }
