b170645228f306c2540a3a10873709a07d86b071
mrkcsc <miguel@miguelgaeta.com>
2014-09-26 19:15:36 -0700

    Draft screen now properly syncs UI to the current draft state - flow is complete.  Should also support restoring UI state on app re-entry.  Initial work into updating draft screen UI with actual player info.

--- app/src/main/java/com/blitz/app/screens/draft/DraftScreen.java
+++ app/src/main/java/com/blitz/app/screens/draft/DraftScreen.java
@@ -12,6 +12,7 @@ import com.blitz.app.utilities.animations.AnimHelperFade;
 import com.blitz.app.utilities.animations.AnimHelperSpringsGroup;
 import com.blitz.app.utilities.animations.AnimHelperSpringsPresets;
 import com.blitz.app.utilities.animations.AnimHelperSpringsView;
+import com.blitz.app.utilities.authentication.AuthHelper;
 import com.blitz.app.utilities.logging.LogHelper;
 import com.blitz.app.view_models.ViewModel;
 import com.blitz.app.view_models.ViewModelDraft;
@@ -56,43 +57,72 @@ public class DraftScreen extends BaseActivity implements ViewModelDraft.ViewMode
     // Page animations.
     private AnimHelperSpringsGroup mAnimations;
 
+    // Flag to see if animations are running.
+    private boolean mAnimationsRunning;
+
+    // Pointer to the current state of the draft.
+    private ViewModelDraft.DraftState mDraftState;
+    private ViewModelDraft.DraftState mDraftStateCurrent;
+
+    // Tracks if player info loaded.
+    private String mPlayer1Id;
+    private String mPlayer2Id;
+
+    private String mPlayer1UserName;
+    private String mPlayer2UserName;
+
+    private int mPlayer1Wins;
+    private int mPlayer1Losses;
+    private int mPlayer1Ties;
+    private int mPlayer1Rating;
+
+    private int mPlayer2Wins;
+    private int mPlayer2Losses;
+    private int mPlayer2Ties;
+    private int mPlayer2Rating;
+
+    private String mPlayer1AvatarUrl;
+    private String mPlayer2AvatarUrl;
+
     // endregion
 
     // region Overwritten Methods
     // =============================================================================================
 
     /**
-     * When screen is created.
+     * Initialize the UI state.
+     *
+     * @param savedInstanceState Instance parameters.
      */
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        playAnimationsIntro();
+        // Blank screen on create.
+        setIntroUIVisibility(false);
     }
 
     /**
-     * Runs when activity has been
-     * presented to the user.
+     * Sync UI on resume.
      */
     @Override
     protected void onResume() {
         super.onResume();
 
-        // Enable animations.
-        mAnimations.enable();
+        syncDraftUIState();
     }
 
     /**
-     * Run custom transitions if needed, also
-     * start timer to detect entering background.
+     * Kill all UI animations on pause.
      */
     @Override
     protected void onPause() {
         super.onPause();
 
+        // Hard stop animations.
+        mAnimationsRunning = false;
+
         // Disable animations.
-        mAnimations.disable();
+        getAnimations().disable();
     }
 
     /**
@@ -117,36 +147,148 @@ public class DraftScreen extends BaseActivity implements ViewModelDraft.ViewMode
     // =============================================================================================
 
     /**
+     * Attempt to set the draft UI state based
+     * on the state of the view model and timing
+     * it with the actual animations.
+     */
+    private void syncDraftUIState() {
+
+        // Do nothing if animating or no state.
+        if (mAnimationsRunning || mDraftState == mDraftStateCurrent) {
+
+            return;
+        }
+
+        switch (mDraftState) {
+            case DRAFT_PREVIEW:
+
+                // If player data is loaded.
+                if (mPlayer1Id != null && mPlayer2Id != null) {
+
+                    // Show intro UI.
+                    playAnimationsIntro();
+                }
+
+                break;
+            case DRAFT_DRAFTING:
+
+                // If in the draft preview state.
+                if (mDraftStateCurrent == ViewModelDraft.DraftState.DRAFT_PREVIEW) {
+
+                    // Hide intro UI and show draft UI.
+                    playAnimationsIntroReversed();
+
+                } else {
+
+                    // Show the draft UI.
+                    playAnimationsDrafting();
+                }
+
+                break;
+            case DRAFT_COMPLETE:
+
+                // Immediately switch state.
+                mDraftStateCurrent = mDraftState;
+
+                // Re-enter the main application.
+                AuthHelper.instance().setCurrentDraft(null);
+                AuthHelper.instance().tryEnterMainApp(this);
+
+                break;
+        }
+    }
+
+    /**
+     * If animations are no longer running,
+     * check to see if we need to update UI state.
+     *
+     * @param animationsRunning Are animations running.
+     */
+    private void setAnimationsRunning(boolean animationsRunning) {
+
+        mAnimationsRunning = animationsRunning;
+
+        if (!mAnimationsRunning) {
+
+            // Animations might require buffering
+            // of the UI state, verify we are
+            // int he correct state.
+            syncDraftUIState();
+        }
+    }
+
+    /**
+     * Lazy load the animations group. There is no single
+     * point of initialization because animations
+     * can start from many entry points.
+     *
+     * @return Animations group, used for bouncy animations.
+     */
+    private AnimHelperSpringsGroup getAnimations() {
+
+        if (mAnimations == null) {
+            mAnimations = AnimHelperSpringsGroup.from(this);
+        }
+
+        return mAnimations;
+    }
+
+    /**
+     * Set visibility of the intro UI.
+     *
+     * @param visible Is visible.
+     */
+    private void setIntroUIVisibility(boolean visible) {
+
+        // Set the intro UI visibility.
+        mDraftIntroContainer
+                .setVisibility(visible ? View.VISIBLE : View.GONE);
+        mDraftHeader
+                .setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    // endregion
+
+    // region Animation Methods
+    // =============================================================================================
+
+    /**
      * Show the intro UI.
      */
     private void playAnimationsIntro() {
 
-        // Create animation group.
-        mAnimations = AnimHelperSpringsGroup.from(this);
+        setAnimationsRunning(true);
+
+        setIntroUIVisibility(true);
 
         // Containers smash.
-        mAnimations.createHelper(20, 4)
+        getAnimations().createHelper(20, 4)
                 .addHelperView(AnimHelperSpringsView.from(mDraftIntroContainerLeft, AnimHelperSpringsPresets.SLIDE_RIGHT))
                 .addHelperView(AnimHelperSpringsView.from(mDraftIntroContainerRight, AnimHelperSpringsPresets.SLIDE_LEFT));
 
         // Header flies down.
-        mAnimations.createHelper(5, 5)
+        getAnimations().createHelper(5, 5)
                 .addHelperView(AnimHelperSpringsView.from(mDraftHeader, AnimHelperSpringsPresets.SLIDE_DOWN));
 
         // Set a springs completion listener.
-        mAnimations.setOnCompleteListener(new Runnable() {
+        getAnimations().setOnCompleteListener(new Runnable() {
 
             @Override
             public void run() {
+
+                // Now in the preview state.
+                mDraftStateCurrent = ViewModelDraft.DraftState.DRAFT_PREVIEW;
 
                 // Fade in the vs text and the spinner.
                 AnimHelperFade.setVisibility(mDraftIntroContainerVs, View.VISIBLE);
                 AnimHelperFade.setVisibility(mDraftLoadingSpinner, View.VISIBLE);
 
-                // Start the draft.
-                // TODO: Uncomment mViewModelDraft.startDrafting();
+                setAnimationsRunning(false);
             }
         });
+
+        // Enable animations.
+        getAnimations().enable();
     }
 
     /**
@@ -154,16 +296,18 @@ public class DraftScreen extends BaseActivity implements ViewModelDraft.ViewMode
      */
     private void playAnimationsIntroReversed() {
 
+        setAnimationsRunning(true);
+
         // Remove current helpers.
-        mAnimations.removeHelpers();
+        getAnimations().removeHelpers();
 
         // Containers smash.
-        mAnimations.createHelper(20, 4)
+        getAnimations().createHelper(20, 4)
                 .addHelperView(AnimHelperSpringsView.from(mDraftIntroContainerLeft, AnimHelperSpringsPresets.SLIDE_RIGHT_REVERSED))
                 .addHelperView(AnimHelperSpringsView.from(mDraftIntroContainerRight, AnimHelperSpringsPresets.SLIDE_LEFT_REVERSED));
 
         // Header flies up.
-        mAnimations.createHelper(5, 5)
+        getAnimations().createHelper(5, 5)
                 .addHelperView(AnimHelperSpringsView.from(mDraftHeader, AnimHelperSpringsPresets.SLIDE_DOWN_REVERSED));
 
         // Fade out vs and spinner.
@@ -171,7 +315,7 @@ public class DraftScreen extends BaseActivity implements ViewModelDraft.ViewMode
         AnimHelperFade.setVisibility(mDraftLoadingSpinner, View.INVISIBLE);
 
         // Set a springs completion listener.
-        mAnimations.setOnCompleteListener(new Runnable() {
+        getAnimations().setOnCompleteListener(new Runnable() {
 
             @Override
             public void run() {
@@ -182,13 +326,15 @@ public class DraftScreen extends BaseActivity implements ViewModelDraft.ViewMode
         });
 
         // Start it up.
-        mAnimations.enable();
+        getAnimations().enable();
     }
 
     /**
      * Show the drafting UI.
      */
     private void playAnimationsDrafting() {
+
+        setAnimationsRunning(true);
 
         // Hide the intro UI.
         mDraftIntroContainer.setVisibility(View.GONE);
@@ -198,26 +344,38 @@ public class DraftScreen extends BaseActivity implements ViewModelDraft.ViewMode
         mDraftContainerDrafting.setVisibility(View.VISIBLE);
 
         // Remove current helpers.
-        mAnimations.removeHelpers();
+        getAnimations().removeHelpers();
 
         // Header flies down.
-        mAnimations.createHelper(5, 5)
+        getAnimations().createHelper(5, 5)
                 .addHelperView(AnimHelperSpringsView.from(mDraftHeader, AnimHelperSpringsPresets.SLIDE_DOWN));
 
         // Containers smash.
-        mAnimations.createHelper(20, 5)
+        getAnimations().createHelper(20, 5)
                 .addHelperView(AnimHelperSpringsView.from(mDraftMatchupPlayerLeft,  AnimHelperSpringsPresets.SLIDE_RIGHT))
                 .addHelperView(AnimHelperSpringsView.from(mDraftMatchupPlayerRight, AnimHelperSpringsPresets.SLIDE_LEFT));
 
         // Players pop in.
-        mAnimations.createHelper(20, 10)
+        getAnimations().createHelper(20, 10)
                 .addHelperView(AnimHelperSpringsView.from(mDraftPlayerTl, AnimHelperSpringsPresets.SCALE_UP))
                 .addHelperView(AnimHelperSpringsView.from(mDraftPlayerTr, AnimHelperSpringsPresets.SCALE_UP))
                 .addHelperView(AnimHelperSpringsView.from(mDraftPlayerBl, AnimHelperSpringsPresets.SCALE_UP))
                 .addHelperView(AnimHelperSpringsView.from(mDraftPlayerBr, AnimHelperSpringsPresets.SCALE_UP));
 
+        getAnimations().setOnCompleteListener(new Runnable() {
+
+            @Override
+            public void run() {
+
+                // Now in the draft state.
+                mDraftStateCurrent = ViewModelDraft.DraftState.DRAFT_DRAFTING;
+
+                setAnimationsRunning(false);
+            }
+        });
+
         // Start it up.
-        mAnimations.enable();
+        getAnimations().enable();
     }
 
     // endregion
@@ -225,22 +383,57 @@ public class DraftScreen extends BaseActivity implements ViewModelDraft.ViewMode
     // region View Model Callbacks
     // =============================================================================================
 
-    @Override
-    public void onDraftingStarted() {
-
-        playAnimationsIntroReversed();
-    }
-
+    /**
+     * Received when a users information is received.
+     *
+     * @param userId Id.
+     * @param userName Username.
+     * @param rating Elo rating.
+     * @param wins Wins.
+     * @param losses Losses.
+     * @param ties Ties.
+     * @param itemAvatarUrl Avatar url.
+     */
     @Override
     public void onUserSynced(String userId, String userName, int rating, int wins, int losses, int ties, String itemAvatarUrl) {
 
-        LogHelper.log("We have user: " + userName + " " + itemAvatarUrl);
+        if (AuthHelper.instance().getUserId().equals(userId)) {
+
+            mPlayer1Id        = userId;
+            mPlayer1UserName  = userName;
+            mPlayer1Rating    = rating;
+            mPlayer1Wins      = wins;
+            mPlayer1Losses    = losses;
+            mPlayer1Ties      = ties;
+            mPlayer1AvatarUrl = itemAvatarUrl;
+
+        } else {
+
+            mPlayer2Id        = userId;
+            mPlayer2UserName  = userName;
+            mPlayer2Rating    = rating;
+            mPlayer2Wins      = wins;
+            mPlayer2Losses    = losses;
+            mPlayer2Ties      = ties;
+            mPlayer2AvatarUrl = itemAvatarUrl;
+        }
+
+        syncDraftUIState();
     }
 
+    /**
+     * Save the draft state on change and
+     * if possible update the UI.
+     *
+     * @param state New draft state.
+     */
     @Override
     public void onDraftStateChanged(ViewModelDraft.DraftState state) {
 
-        LogHelper.log("Draft state changed: " + state);
+        // Update state.
+        mDraftState = state;
+
+        syncDraftUIState();
     }
 
     // endregion
