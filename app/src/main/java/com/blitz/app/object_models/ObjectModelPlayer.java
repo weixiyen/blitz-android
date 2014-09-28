48548a3809752d246caecb06585ee96712dc7392
mrkcsc <miguel@miguelgaeta.com>
2014-09-28 00:22:31 -0700

    Added fetch player to object model player.

--- app/src/main/java/com/blitz/app/object_models/ObjectModelPlayer.java
+++ app/src/main/java/com/blitz/app/object_models/ObjectModelPlayer.java
@@ -1,6 +1,13 @@
 package com.blitz.app.object_models;
 
+import android.app.Activity;
+
+import com.blitz.app.utilities.rest.RestAPICallback;
+import com.blitz.app.utilities.rest.RestAPIResult;
+import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
 
 /**
  * Created by Miguel on 9/27/2014. Copyright 2014 Blitz Studios
@@ -24,4 +31,82 @@ public class ObjectModelPlayer extends ObjectModel {
 
     @SuppressWarnings("unused") @SerializedName("is_home_team")
     private boolean mIsHomeTeam;
+
+    // region REST Methods
+    // =============================================================================================
+
+    @SuppressWarnings("unused")
+    public static void fetchPlayers(Activity activity,
+                                    ArrayList<String> playerIds,
+                                    final CallbackPlayers callback) {
+
+    }
+
+    /**
+     * Fetch an NFL player.
+     *
+     * @param activity Used for dialogs.
+     * @param playerId Player id.
+     * @param callback Callback.
+     */
+    @SuppressWarnings("unused")
+    public static void fetchPlayer(Activity activity,
+                                   String playerId,
+                                   final CallbackPlayer callback) {
+
+        if (playerId == null) {
+            return;
+        }
+
+        RestAPICallback<RestAPIResult<ObjectModelPlayer>> operation =
+                new RestAPICallback<RestAPIResult<ObjectModelPlayer>>(activity) {
+
+                    @Override
+                    public void success(RestAPIResult<ObjectModelPlayer> jsonObject) {
+
+                        // Now left queue.
+                        if (callback != null) {
+                            callback.onSuccess(jsonObject.getResult());
+                        }
+                    }
+                };
+
+        // Make api call.
+        mRestAPI.nfl_player_get(playerId, operation);
+    }
+
+    @SuppressWarnings("unused")
+    public static void fetchPlayerFromCometJson(Activity activity,
+                                                JsonObject cometJson,
+                                                final CallbackPlayer callback) {
+
+    }
+
+    // endregion
+
+    // region Public Methods
+    // =============================================================================================
+
+    @SuppressWarnings("unused")
+    public String fetchPhotoUrl() {
+
+        return null;
+    }
+
+    // endregion
+
+    // region Callbacks
+    // =============================================================================================
+
+    public interface CallbackPlayer {
+
+        public void onSuccess(ObjectModelPlayer draft);
+    }
+
+    public interface CallbackPlayers {
+
+        public void onSuccess(ObjectModelPlayer draft);
+    }
+
+    // endregion
 }
