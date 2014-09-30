991b87767b2d168456106f2d2c3f6b14c7a53068
mrkcsc <miguel@miguelgaeta.com>
2014-09-29 23:20:28 -0700

    Added method to patch user avatar id.

--- app/src/main/java/com/blitz/app/object_models/ObjectModelUser.java
+++ app/src/main/java/com/blitz/app/object_models/ObjectModelUser.java
@@ -39,6 +39,205 @@ public class ObjectModelUser extends ObjectModel {
 
     // endregion
 
+    // region REST Calls
+    // =============================================================================================
+
+    /**
+     * Update the users avatar.
+     *
+     * @param activity Activity for dialogs.
+     * @param avatarId Avatar id.
+     * @param callback Callback.
+     */
+    @SuppressWarnings("unused")
+    public static void updateAvatar(Activity activity, String avatarId,
+                                    final CallbackUser callback) {
+        if (avatarId == null) {
+            return;
+        }
+
+        RestAPICallback<RestAPIResult<ObjectModelUser>> operation =
+                new RestAPICallback<RestAPIResult<ObjectModelUser>>(activity) {
+
+                    @Override
+                    public void success(RestAPIResult<ObjectModelUser> jsonObject) {
+
+                        if (callback != null) {
+                            callback.onSuccess(jsonObject.getResult());
+                        }
+                    }
+                };
+
+        // Create object holding values to replace.
+        JsonObject replace = new JsonObject();
+
+        replace.addProperty("avatar_id", avatarId);
+
+        // Create body.
+        JsonObject body = new JsonObject();
+
+        // Add replace object.
+        body.add("replace", replace);
+
+        // Make rest call for code.
+        mRestAPI.user_patch(body, operation);
+    }
+
+    /**
+     * Get and populate the user model - requires a user
+     * id which means the user must be logged in.
+     */
+    @SuppressWarnings("unused")
+    public static void getUser(Activity activity, String userId,
+                               final CallbackUser callback, boolean logoutOnFailure) {
+
+        // Rest operation.
+        RestAPICallback<RestAPIResult<ObjectModelUser>> operation =
+                new RestAPICallback<RestAPIResult<ObjectModelUser>>(activity) {
+
+                    @Override
+                    public void success(RestAPIResult<ObjectModelUser> jsonObject) {
+
+                        if (callback != null) {
+                            callback.onSuccess(jsonObject.getResult());
+                        }
+                    }
+                };
+
+        operation.setLogoutOnFailure(logoutOnFailure);
+
+        // Make api call to fetch user data.
+        mRestAPI.user_get(userId, operation);
+    }
+
+    /**
+     * Fetch a lust of users (by user id).
+     *
+     * @param activity Activity for dialogs.
+     * @param userIds List of user id's.
+     * @param callback Callback.
+     */
+    @SuppressWarnings("unused")
+    public static void getUsers(Activity activity, List<String> userIds,
+                                final CallbackUsers callback) {
+
+        RestAPICallback<RestAPIResult<ObjectModelUser>> operation =
+                new RestAPICallback<RestAPIResult<ObjectModelUser>>(activity) {
+
+                    @Override
+                    public void success(RestAPIResult<ObjectModelUser> jsonObject) {
+
+                        if (callback != null) {
+                            callback.onSuccess(jsonObject.getResults());
+                        }
+                    }
+                };
+
+        // Pluck a subset of the fields.
+        ArrayList<String> pluck = new ArrayList<String>();
+
+        pluck.add("id");
+        pluck.add("username");
+        pluck.add("status");
+        pluck.add("avatar_id");
+        pluck.add("rating");
+        pluck.add("wins");
+        pluck.add("losses");
+        pluck.add("ties");
+        pluck.add("cash");
+
+        mRestAPI.users_get(userIds, "id", pluck, null, null, operation);
+    }
+
+    /**
+     * Sign up (register) a user.
+     *
+     * @param activity Activity for dialogs.
+     * @param callback Callback.
+     */
+    @SuppressWarnings("unused")
+    public void signUp(Activity activity, final CallbackSignUp callback) {
+
+        // Rest operation.
+        RestAPICallback<JsonObject> operation =
+                new RestAPICallback<JsonObject>(activity) {
+
+                    @Override
+                    public void success(JsonObject jsonObject) {
+
+                        if (jsonObject != null) {
+
+                            // Fetch user object.
+                            JsonObject result = jsonObject.getAsJsonObject("result");
+
+                            // Sign in the user.
+                            AuthHelper.instance().signIn(
+                                    result.get("id").getAsString(),
+                                    result.get("username").getAsString(), mEmail, mPassword);
+
+                            // Now signed up.
+                            callback.onSignUp();
+                        }
+                    }
+                };
+
+        operation.setIsAuthentication(true);
+
+        // Construct POST body.
+        JsonObject body = new JsonObject();
+        body.addProperty("email", mEmail);
+        body.addProperty("username", mUsername);
+        body.addProperty("password", mPassword);
+
+        // Make rest call for code.
+        mRestAPI.users_post(body, operation);
+    }
+
+    /**
+     * Sign in the user.
+     *
+     * @param activity Activity for dialogs.
+     * @param callback Callback.
+     */
+    @SuppressWarnings("unused")
+    public void signIn(Activity activity, final CallbackSignIn callback) {
+
+        // Rest operation.
+        RestAPICallback<JsonObject> operation =
+                new RestAPICallback<JsonObject>(activity) {
+
+                    @Override
+                    public void success(JsonObject jsonObject) {
+
+                        if (jsonObject != null) {
+
+                            // Fetch user object.
+                            JsonObject user = jsonObject.getAsJsonObject("user");
+
+                            // Sign in the user.
+                            AuthHelper.instance().signIn(
+                                    user.get("id").getAsString(),
+                                    user.get("username").getAsString(), mEmail, mPassword);
+
+                            // Now signed in.
+                            callback.onSignIn();
+                        }
+                    }
+                };
+
+        operation.setIsAuthentication(true);
+
+        // Construct POST body.
+        JsonObject body = new JsonObject();
+        body.addProperty("username", mUsername);
+        body.addProperty("password", mPassword);
+
+        // Make auth rest call.
+        mRestAPI.auth_post(body, operation);
+    }
+
+    // endregion
+
     // region Public Methods
     // =============================================================================================
 
@@ -145,156 +344,6 @@ public class ObjectModelUser extends ObjectModel {
     public void setPassword(EditText password) {
 
         mPassword = password.getText().toString();
-    }
-
-    /**
-     * Get and populate the user model - requires a user
-     * id which means the user must be logged in.
-     */
-    @SuppressWarnings("unused")
-    public static void getUser(Activity activity, String userId,
-                               final CallbackUser callback, boolean logoutOnFailure) {
-
-        // Rest operation.
-        RestAPICallback<RestAPIResult<ObjectModelUser>> operation =
-                new RestAPICallback<RestAPIResult<ObjectModelUser>>(activity) {
-
-            @Override
-            public void success(RestAPIResult<ObjectModelUser> jsonObject) {
-
-                if (callback != null) {
-                    callback.onSuccess(jsonObject.getResult());
-                }
-            }
-        };
-
-        operation.setLogoutOnFailure(logoutOnFailure);
-
-        // Make api call to fetch user data.
-        mRestAPI.user_get(userId, operation);
-    }
-
-    /**
-     * Fetch a lust of users (by user id).
-     *
-     * @param activity Activity for dialogs.
-     * @param userIds List of user id's.
-     * @param callback Callback.
-     */
-    @SuppressWarnings("unused")
-    public static void getUsers(Activity activity, List<String> userIds, final CallbackUsers callback) {
-
-        RestAPICallback<RestAPIResult<ObjectModelUser>> operation =
-                new RestAPICallback<RestAPIResult<ObjectModelUser>>(activity) {
-
-            @Override
-            public void success(RestAPIResult<ObjectModelUser> jsonObject) {
-
-                if (callback != null) {
-                    callback.onSuccess(jsonObject.getResults());
-                }
-            }
-        };
-
-        // Pluck a subset of the fields.
-        ArrayList<String> pluck = new ArrayList<String>();
-
-        pluck.add("id");
-        pluck.add("username");
-        pluck.add("status");
-        pluck.add("avatar_id");
-        pluck.add("rating");
-        pluck.add("wins");
-        pluck.add("losses");
-        pluck.add("ties");
-        pluck.add("cash");
-
-        mRestAPI.users_get(userIds, "id", pluck, null, null, operation);
-    }
-
-    /**
-     * Sign up (register) a user.
-     *
-     * @param activity Activity for dialogs.
-     * @param callback Callback.
-     */
-    public void signUp(Activity activity, final CallbackSignUp callback) {
-
-        // Rest operation.
-        RestAPICallback<JsonObject> operation =
-                new RestAPICallback<JsonObject>(activity) {
-
-            @Override
-            public void success(JsonObject jsonObject) {
-
-                if (jsonObject != null) {
-
-                    // Fetch user object.
-                    JsonObject result = jsonObject.getAsJsonObject("result");
-
-                    // Sign in the user.
-                    AuthHelper.instance().signIn(
-                            result.get("id").getAsString(),
-                            result.get("username").getAsString(), mEmail, mPassword);
-
-                    // Now signed up.
-                    callback.onSignUp();
-                }
-            }
-        };
-
-        operation.setIsAuthentication(true);
-
-        // Construct POST body.
-        JsonObject body = new JsonObject();
-                   body.addProperty("email", mEmail);
-                   body.addProperty("username", mUsername);
-                   body.addProperty("password", mPassword);
-
-        // Make rest call for code.
-        mRestAPI.users_post(body, operation);
-    }
-
-    /**
-     * Sign in the user.
-     *
-     * @param activity Activity for dialogs.
-     * @param callback Callback.
-     */
-    public void signIn(Activity activity, final CallbackSignIn callback) {
-
-        // Rest operation.
-        RestAPICallback<JsonObject> operation =
-                new RestAPICallback<JsonObject>(activity) {
-
-            @Override
-            public void success(JsonObject jsonObject) {
-
-                if (jsonObject != null) {
-
-                    // Fetch user object.
-                    JsonObject user = jsonObject.getAsJsonObject("user");
-
-                    // Sign in the user.
-                    AuthHelper.instance().signIn(
-                            user.get("id").getAsString(),
-                            user.get("username").getAsString(), mEmail, mPassword);
-
-                    // Now signed in.
-                    callback.onSignIn();
-                }
-            }
-        };
-
-        operation.setIsAuthentication(true);
-
-        // Construct POST body.
-        JsonObject body = new JsonObject();
-                   body.addProperty("username", mUsername);
-                   body.addProperty("password", mPassword);
-
-        // Make auth rest call.
-        mRestAPI.auth_post(body, operation);
     }
 
     // endregion
