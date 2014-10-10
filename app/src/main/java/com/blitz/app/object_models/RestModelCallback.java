2e00ce2a3bfc4b12473a655e66cd41239dfea2a9
mrkcsc <miguel@miguelgaeta.com>
2014-10-09 21:05:35 -0700

    Added generalized RestModel (to be renamed from ObjectModel) callbacks.

---
+++ app/src/main/java/com/blitz/app/object_models/RestModelCallback.java
@@ -0,0 +1,20 @@
+package com.blitz.app.object_models;
+
+/**
+ * Created by mrkcsc on 10/9/14. Copyright 2014 Blitz Studios
+ */
+public abstract class RestModelCallback<T extends ObjectModel> {
+
+    /**
+     * Emit response object on success
+     * where object is a subclass of RestModel.
+     *
+     * @param object Object result.
+     */
+    public abstract void onSuccess(T object);
+
+    @SuppressWarnings("unused")
+    public void onFailure() {
+
+    }
+}
---
+++ app/src/main/java/com/blitz/app/object_models/RestModelCallbacks.java
@@ -0,0 +1,22 @@
+package com.blitz.app.object_models;
+
+import java.util.List;
+
+/**
+ * Created by mrkcsc on 10/9/14. Copyright 2014 Blitz Studios
+ */
+public abstract class RestModelCallbacks<T extends ObjectModel> {
+
+    /**
+     * Emit response object list on success
+     * where object is a subclass of RestModel.
+     *
+     * @param object List of object result.
+     */
+    public abstract void onSuccess(List<T> object);
+
+    @SuppressWarnings("unused")
+    public void onFailure() {
+
+    }
+}
