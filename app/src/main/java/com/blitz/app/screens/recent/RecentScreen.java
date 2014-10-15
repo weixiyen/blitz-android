e5ce18b321d1ce21554ac65e39a6c9931a835c9d
mrkcsc <miguel@miguelgaeta.com>
2014-10-14 22:12:30 -0700

    Recent screen now works again but scrubber is not yet implemented.

--- app/src/main/java/com/blitz/app/screens/recent/RecentScreen.java
+++ app/src/main/java/com/blitz/app/screens/recent/RecentScreen.java
@@ -1,8 +1,6 @@
 package com.blitz.app.screens.recent;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -39,7 +37,6 @@ public class RecentScreen extends BaseFragment implements ViewModelRecent.Callba
     // region Overwritten Methods
     // =============================================================================================
 
-
     /**
      * Setup the recent matches UI.
      */
@@ -48,12 +45,27 @@ public class RecentScreen extends BaseFragment implements ViewModelRecent.Callba
         super.onCreateView(savedInstanceState);
 
         mScrubber.setViewPager(((MainScreen)getActivity()).getViewPager());
+        mScrubber.setSize(WEEKS_IN_SEASON);
+    }
+
+    @Override
+    public ViewModel onFetchViewModel() {
+        if(mViewModel == null) {
+            mViewModel = new ViewModelRecent(getBaseActivity(), this);
+        }
+        return mViewModel;
     }
 
     @Override
     public void onDrafts(List<HeadToHeadDraft> matches, ViewModelRecent.Summary summary, int week) {
 
-        //setActiveWeekIndicator(week);
+        if (mRecentHeader != null) {
+            mRecentHeader.setText("Week " + week);
+        }
+
+        if (mScrubber != null) {
+            mScrubber.setScrubberItemSelected(week);
+        }
 
         final RecentScreenMatchAdapter adapter = new RecentScreenMatchAdapter(matches, getActivity());
 
@@ -82,68 +94,6 @@ public class RecentScreen extends BaseFragment implements ViewModelRecent.Callba
         }
         String amount = String.format("$%.2f", Math.abs(cents / 100f));
         return sign + amount;
-    }
-
-    // poor man's closure
-    private static abstract class WeekSettingListener implements View.OnClickListener {
-        final int mWeek;
-        WeekSettingListener(int week) {
-            mWeek = week;
-        }
-    }
-
-    /**
-     * Setup the week scrubber.
-     */
-    private void setupScrubber() {
-
-        for (int i = 0; i < WEEKS_IN_SEASON; i++) {
-
-            // Inflate the week view.
-            LayoutInflater.from(getActivity())
-                    .inflate(R.layout.recent_screen_week, mScrubber, true);
-        }
-
-        for (int i = 0; i < WEEKS_IN_SEASON; i++) {
-            TextView weekPicker = (TextView) mScrubber.getChildAt(i);
-
-            weekPicker.setOnClickListener(new WeekSettingListener(i + 1) {
-                @Override
-                public void onClick(View view) {
-
-                    // Immediately update the page indicator to make
-                    // the UI feel more responsive.
-                    setActiveWeekIndicator(mWeek);
-
-                    // Update the data, which is asynchronous
-                    mViewModel.updateWeek(mWeek);
-                }
-            });
-        }
-    }
-
-    private void setActiveWeekIndicator(int week) {
-
-        for(int j=0; j < WEEKS_IN_SEASON; j++) {
-
-            TextView indicator = (TextView) mScrubber.getChildAt(j);
-            if(j + 1 == week) {
-                indicator.setTextColor(getResources().getColor(R.color.active_blue));
-            } else {
-                indicator.setTextColor(getResources().getColor(R.color.text_color_light));
-            }
-        }
-
-        //((TextView)getActivity().findViewById(R.id.main_recent_header)).setText("Week " + week);
-    }
-
-
-    @Override
-    public ViewModel onFetchViewModel() {
-        if(mViewModel == null) {
-            mViewModel = new ViewModelRecent(getBaseActivity(), this);
-        }
-        return mViewModel;
     }
 
     // endregion
--- app/src/main/java/com/blitz/app/screens/recent/RecentScreenScrubber.java
+++ app/src/main/java/com/blitz/app/screens/recent/RecentScreenScrubber.java
@@ -3,11 +3,17 @@ package com.blitz.app.screens.recent;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.blitz.app.R;
 import com.blitz.app.utilities.logging.LogHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mrkcsc on 10/14/14. Copyright 2014 Blitz Studios
@@ -15,42 +21,151 @@ import com.blitz.app.utilities.logging.LogHelper;
 public class RecentScreenScrubber extends LinearLayout {
 
     private ViewPager mViewPager;
+    private int mScrubberSize;
+    private Integer mScrubberItemSelected;
+    private List<TextView> mScrubberItems;
 
+    // region Constructors
+    // =============================================================================================
+
+    @SuppressWarnings("unused")
     public RecentScreenScrubber(Context context) {
         super(context);
 
         setupTouchEvents();
     }
 
+    @SuppressWarnings("unused")
     public RecentScreenScrubber(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         setupTouchEvents();
     }
 
+    @SuppressWarnings("unused")
     public RecentScreenScrubber(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
         setupTouchEvents();
     }
 
+    // endregion
+
+    // region Overwritten Methods
+    // =============================================================================================
+
+    /**
+     * Tell the scrubber that we want to intercept
+     * all touch events and not give it to the children.
+     *
+     * @param ev Motion event.
+     *
+     * @return Always true to eat the event.
+     */
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        // do whatever you want with the event
-        // and return true so that children don't receive it
-        //LogHelper.log("TOuched 2");
 
         return true;
     }
 
+    // endregion
+
+    // region Public Methods
+    // =============================================================================================
+
+    /**
+     * Provide a view pager if this scrubber
+     * lives inside one.  Horizontal scrolling will
+     * be disabled when touching the scrubber.
+     *
+     * @param viewPager Target view pager.
+     */
     public void setViewPager(ViewPager viewPager) {
 
         mViewPager = viewPager;
     }
 
+    /**
+     * Set the size of the scrubber.
+     *
+     * @param scrubberSize Amount of items to scrub.
+     */
+    public void setSize(int scrubberSize) {
+
+        // Update the size.
+        mScrubberSize = scrubberSize;
+
+        // Setup the Items.
+        setupScrubberItems();
+    }
+
+    /**
+     * Set a scrubber item to be selected.
+     *
+     * @param position Scrubber item index.
+     */
+    public void setScrubberItemSelected(int position) {
+
+        // if valid position is provided.
+        if (position < mScrubberSize && position > 0) {
+
+            // Set selected color.
+            mScrubberItems.get(position)
+                    .setTextColor(getResources().getColor(R.color.active_blue));
+
+            if (mScrubberItemSelected != null) {
+
+                // Reset color of old selected.
+                mScrubberItems.get(mScrubberItemSelected)
+                        .setText(getResources().getColor(R.color.text_color_light));
+            }
+
+            // Update selected index.
+            mScrubberItemSelected = position;
+        }
+    }
+
+    // endregion
+
+    // region Private Methods
+    // =============================================================================================
+
+    /**
+     * Inflate scrubber items into the view.
+     */
+    private void setupScrubberItems() {
+
+        if (mScrubberItems == null) {
+            mScrubberItems = new ArrayList<TextView>();
+        }
+
+        // Remove older items.
+        mScrubberItems.clear();
+
+        for (int i = 0; i < mScrubberSize; i++) {
+
+            // Inflate scrubber item.
+            LayoutInflater.from(getContext())
+                    .inflate(R.layout.recent_screen_week, this, true);
+        }
+
+        // Loop again to get inflated children.
+        for (int i = 0; i < mScrubberSize; i++) {
+
+            // Add to array of scrubber item views.
+            mScrubberItems.add((TextView)getChildAt(i));
+        }
+    }
+
+    /**
+     * Scrubber intercepts touch events and if inside a view
+     * pager, disabled swiping during that time.  Processes
+     * the scrub event as horizontal movement.
+     */
     private void setupTouchEvents() {
 
         setOnTouchListener(new View.OnTouchListener() {
+
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
 
@@ -86,4 +201,6 @@ public class RecentScreenScrubber extends LinearLayout {
 
         LogHelper.log("Scrubbed: " + xPosScrubberStart + " position current: " + xPosCurrent);
     }
+
+    // endregion
 }
