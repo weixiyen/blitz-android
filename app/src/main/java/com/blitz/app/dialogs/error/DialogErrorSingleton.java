f2fef2072c84b8140f7a9427b563d74bd741e8c6
mrkcsc <miguel@miguelgaeta.com>
2014-11-03 00:15:08 -0800

    Initial singleton.

---
+++ app/src/main/java/com/blitz/app/dialogs/error/DialogErrorSingleton.java
@@ -0,0 +1,64 @@
+package com.blitz.app.dialogs.error;
+
+import android.support.v4.app.FragmentManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Miguel on 11/2/2014. Copyright 2014 Blitz Studios
+ */
+public class DialogErrorSingleton {
+
+    // region Member Variables
+    // ============================================================================================================
+
+    // Instance object.
+    private static DialogErrorSingleton mInstance;
+
+    // Single error dialog.
+    private DialogError mDialogError;
+
+    // Most recent instance of fragment manager.
+    private FragmentManager mFragmentManager;
+
+    private List<Integer> mErrorMessageList;
+
+    // endregion
+
+    public static DialogErrorSingleton instance() {
+
+        if (mInstance == null) {
+            synchronized (DialogErrorSingleton.class) {
+
+                if (mInstance == null) {
+                    mInstance = new DialogErrorSingleton();
+
+                    // Initialize list of error dialogs to show.
+                    mInstance.mErrorMessageList = new ArrayList<Integer>();
+
+                    // Initialize the internal error dialog.
+                    mInstance.mDialogError = new DialogError();
+                    mInstance.mDialogError.setOnDismissAction(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            if (mInstance.mErrorMessageList.size() > 0) {
+                                mInstance.mErrorMessageList.remove(mInstance.mErrorMessageList.size() - 1);
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
+        return mInstance;
+    }
+
+    public void show(FragmentManager fragmentManager) {
+
+        // Set fragment manager.
+        mFragmentManager = fragmentManager;
+    }
+}
