fff4efe0acd0f3e79fe1b9137ed45086a89cd100
Nate Black <spiffy1911@gmail.com>
2014-11-05 22:55:00 -0800

    Added deposit screen

---
+++ app/src/main/java/com/blitz/app/screens/payments/DepositScreen.java
@@ -0,0 +1,11 @@
+package com.blitz.app.screens.payments;
+
+import com.blitz.app.utilities.android.BaseActivity;
+
+/**
+ * Screen for depositing money.
+ *
+ * Created by Nate on 11/5/14.
+ */
+public class DepositScreen extends BaseActivity {
+}
